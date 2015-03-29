59440a35310127e8cbf6c688aa0878484ca6ed6f
zhouhaibing089 <zhouhaibing089@gmail.com>
2015-03-29 18:34:59 +0800

    string value redefine

--- app/src/main/java/seaice/app/groupcontact/UserAddActivity.java
+++ app/src/main/java/seaice/app/groupcontact/UserAddActivity.java
@@ -1,6 +1,7 @@
 package seaice.app.groupcontact;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.EditText;
 
 import javax.inject.Inject;
@@ -33,6 +34,8 @@ public class UserAddActivity extends DaggerActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_add);
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         ButterKnife.inject(this);
     }
 
@@ -52,5 +55,16 @@ public class UserAddActivity extends DaggerActivity {
                 // finish();
             }
         });
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
